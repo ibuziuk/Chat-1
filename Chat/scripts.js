c2f06b89ebca056e7b4ef80402fc6158c96bb040
Vitaly Molochko <vitalymolochko@gmail.com>
2015-03-15 20:29:41 +0300

    add save in localStorage

--- Chat/scripts.js
+++ Chat/scripts.js
@@ -1,24 +1,105 @@
 
+var messageList = [];
+
+var uniqueId = function() {
+    var date = Date.now();
+    var random = Math.random() * Math.random();
+
+    return Math.floor(date * random).toString();
+};
+
+var theMessage = function(text, name , done) {
+    return {
+        nick: name,
+        message: text,
+        status: !!done,
+        id: uniqueId()
+    };
+};
+
+function createAllTasks(allTasks) {
+    for(var i = 0; i < allTasks.length; i++){
+        if(allTasks[i].status != false){
+            task = allTasks[i];
+            messageDiv = $('.exampleMessage').first().clone();
+            messageDiv.find('.nick').html(task.nick + ":");
+            messageDiv.find('.message').html(task.message);
+            messageDiv.attr('message-id', task.id);
+            messageDiv.attr('status', task.status);
+            $('#showMessage').append(messageDiv.show());
+        }
+        messageList.push(allTasks[i]);
+    }
+}
+
+function store(listToSave) {
+
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    localStorage.setItem("Chat messageList", JSON.stringify(listToSave));
+}
+
+function restoreMessages() {
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    var item = localStorage.getItem("Chat messageList");
+
+    return item && JSON.parse(item);
+}
+
+function restoreName(){
+    if(typeof(Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+
+    var item = localStorage.getItem("Chat userName");
+
+
+    $('#messageArea').attr('disabled', false);
+    $('#send').attr('disabled', false);
+
+    return item && JSON.parse(item);
+}
+
+
 $(document).ready(function () {
 
     $userName = $('h4.currentName');
     $inputChange = $('#changeName');
 
+    var allMessages = restoreMessages() || [];
+    createAllTasks(allMessages);
+    $userName.html(restoreName());
+
+    if($userName.html()!=""){
+        debugger;
+        li = $('#onlineArea li').first().clone();
+        li.html($userName.html());
+        $('#onlineArea').append(li.show());
+
+    }
+
     //enter in chat
-    $('#submitUser').click(function () {
+    $('#submitUser').click(function () {    
         if ($('#userLogin').val() != "") {
             userName = $('#userLogin').val();
             $userName.html(userName);
             $('#userLogin').val('');
             $('#messageArea').attr('disabled', false);
             $('#send').attr('disabled', false);
-
             debugger;
-
             li = $('#onlineArea li').first().clone();
             li.html($userName.html());
-            $('#onlineArea').append(li);
+            $('#onlineArea').append(li.show());
 
+            localStorage.setItem("Chat userName", JSON.stringify($userName.html()));
         };
     })
 
@@ -43,12 +124,25 @@ $(document).ready(function () {
         $(this).hide();
         $('#changeCurrentName').show()
         $userName.show();
+
+        localStorage.setItem("Chat userName", JSON.stringify($userName.html()));
         };
     })
 
     //delete message
     $('#showMessage').on('click', 'button.close', function () {
-        $(this).parent().remove();
+        id = $(this).closest('.exampleMessage').attr('message-id');
+        $(this).parent().parent().remove();
+
+        for(var i = 0; i < messageList.length; i++) {
+            if(messageList[i].id != id)
+                continue;
+
+            messageList[i].status = false;
+            store(messageList);
+
+            return;
+        }
     })
 
 
@@ -56,17 +150,22 @@ $(document).ready(function () {
     $('#send').click(function () {
         message = $('#messageArea').val();
         if (message != "") {
-            messageDiv = $('#showMessage p').first().clone();
-            messageDiv.find('.nick').html($userName.html());
+            task = theMessage(message,$userName.html(),true);
+            messageList.push(task);
+            messageDiv = $('.exampleMessage').first().clone();
+            messageDiv.find('.nick').html($userName.html() + ":");
             messageDiv.find('.message').html(message);
-            $('#showMessage').append(messageDiv);
+            messageDiv.attr('message-id', uniqueId());
+            messageDiv.attr('status', true);
+            $('#showMessage').append(messageDiv.show());
             $('#messageArea').val('');
         };
+        store(messageList);
     })
 
     //edit message
     $('#showMessage').on('click', 'a.editMessage', function () {
-        $p = $(this).closest('p')
+        $p = $(this).closest('.exampleMessage')
         $message = $p.find('.message')
         $input = $p.find('#changeMessage')
         editer = $message.html();
@@ -77,13 +176,11 @@ $(document).ready(function () {
         $input.show();
         $(this).hide();
         $p.find('#saveMessage').show();
-
     })
 
     //save edit message
     $('#showMessage').on('click', 'button#saveMessage', function () {
-        debugger
-        $p = $(this).closest('p')
+        $p = $(this).closest('.exampleMessage')
         $input = $p.find('#changeMessage')
         editer = $input.val();
         $input.attr('disabled', true);
@@ -92,8 +189,20 @@ $(document).ready(function () {
         $p.find('.nick').show();
         $(this).hide();
         $p.find('a.editMessage').show();
+
+        id = $(this).closest('.exampleMessage').attr('message-id');
+        for(var i = 0; i < messageList.length; i++) {
+            if(messageList[i].id != id)
+                continue;
+
+            messageList[i].message = editer;
+            store(messageList);
+
+            return;
+        }
     })
 
+    
 })
 
 
